90f9691ce287b23ea1dc35a3762957a1c2185c2d
thisismybday <thisismybday@gmail.com>
2019-05-28 23:39:16 +0900

    modify

---
+++ build/precache-manifest.419dce0f6cc2396a7f90cd2fd3303ec1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7f71abec6c077e541542a99825e4cb",
+    "url": "/b-day/index.html"
+  },
+  {
+    "revision": "a4efdadd55ea979c9cd6",
+    "url": "/b-day/static/css/main.9214b785.chunk.css"
+  },
+  {
+    "revision": "dfba4194e8c4a468fac0",
+    "url": "/b-day/static/js/2.28cf491e.chunk.js"
+  },
+  {
+    "revision": "a4efdadd55ea979c9cd6",
+    "url": "/b-day/static/js/main.b177909f.chunk.js"
+  },
+  {
+    "revision": "9d94a65f02df55a21bce",
+    "url": "/b-day/static/js/runtime~main.3eff5629.js"
+  }
+]);
